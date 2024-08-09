16574d59a873f60db016327c6c49a8003cf14f75
Kirill Dubov <31469909+KirillUnited@users.noreply.github.com>
2024-08-09 17:01:23 +0300

    Add getInstagramData util

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,10 +21,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={cn(
-        "min-h-screen bg-background font-sans antialiased",
         fontSans.variable
       )}>
-        {children}
+        <div className={cn(
+          "wrapper"
+        )}>
+          {children}
+        </div>
       </body>
     </html>
   );
