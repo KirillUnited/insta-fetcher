2913ebca11661364aabf766f40ddfa08a5bac63e
Kirill Dubov <31469909+KirillUnited@users.noreply.github.com>
2024-08-13 13:48:10 +0300

    Add avatar shadcn component

--- src/components/shared/Header.tsx
+++ src/components/shared/Header.tsx
@@ -2,6 +2,7 @@ import { cn } from '@/lib/utils'
 import React from 'react';
 import { ThemeSwitcher } from '@/components/shared';
 import { SearchInput } from '@/components/shared';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 
 interface Props extends React.HTMLAttributes<HTMLElement> {
 }
@@ -11,7 +12,13 @@ export default function Header({ className }: Props) {
         <header className={cn("header", className)}>
             <div className='container'>
                 <div className='flex items-center justify-between min-h-20 py-2'>
-                    <h2 className='text-2xl font-bold'>@san_vino</h2>
+                    <div className='flex items-center gap-4'>
+                        <Avatar>
+                            <AvatarImage src="/images/group-avatar.jpg" alt='@san_vino' />
+                            <AvatarFallback>@san_vino</AvatarFallback>
+                        </Avatar>
+                        <p className='text-xl font-bold'>@san_vino</p>
+                    </div>
                     <div className='flex items-center gap-4'>
                         <SearchInput variant={'withIcon'} />
                         <ThemeSwitcher theme='light' />
