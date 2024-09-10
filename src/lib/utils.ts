d554ff21f3ae70a965bd9e71797482a0835e98e2
Kirill Dubov <31469909+KirillUnited@users.noreply.github.com>
2024-09-10 18:48:12 +0300

    refactor: :fire: Remove getInstagramData func

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,20 +4,3 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
-
-// fetch the Instagram followers
-import axios from 'axios';
-
-export async function getInstagramData(username: string) {
-  try {
-    const response = await axios.get(`https://www.instagram.com/${username}/?__a=1`);
-    // const followersCount = response.data.graphql.user.edge_followed_by.count;
-    const data = response.data;
-
-    return data;
-  } catch (error) {
-    console.error('Error fetching Instagram:', error);
-
-    return null;
-  }
-}
