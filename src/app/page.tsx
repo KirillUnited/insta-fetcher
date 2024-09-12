cd3772aac424334077d678b9c0c0680725e28f5b
Kirill Dubov <31469909+KirillUnited@users.noreply.github.com>
2024-09-12 12:05:12 +0300

    feat: :sparkles: Add users API

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,26 @@
 import { HeroSection } from "@/components/shared";
 import { DataSection, DataTable } from "@/components/shared/data";
+import { Loader2 } from "lucide-react";
+import { Suspense } from "react";
 
 export default async function Home() {
+  let data = [];
+
+  try {
+    data = await fetch("http://localhost:3000/api/users", {
+      method: "GET",
+    }).then((res) => res.json());
+  } catch (error) {
+    console.log(error);
+  }
+
   return (
     <>
       <HeroSection />
       <DataSection>
-        <DataTable />
+        <Suspense fallback={<Loader2 className="w-10 h-10 animate-spin" />}>
+          <DataTable data={data} />
+        </Suspense>
       </DataSection>
     </>
   );
--- src/components/shared/data/DataTable.tsx
+++ src/components/shared/data/DataTable.tsx
@@ -35,47 +35,17 @@ import {
   TableRow,
 } from "@/components/ui/table"
 
-const data: Payment[] = [
-  {
-    id: "m5gr84i9",
-    amount: 316,
-    status: "success",
-    email: "ken99@yahoo.com",
-  },
-  {
-    id: "3u1reuv4",
-    amount: 242,
-    status: "success",
-    email: "Abe45@gmail.com",
-  },
-  {
-    id: "derv1ws0",
-    amount: 837,
-    status: "processing",
-    email: "Monserrat44@gmail.com",
-  },
-  {
-    id: "5kma53ae",
-    amount: 874,
-    status: "success",
-    email: "Silas22@gmail.com",
-  },
-  {
-    id: "bhqecj4p",
-    amount: 721,
-    status: "failed",
-    email: "carmella@hotmail.com",
-  },
-]
-
-export type Payment = {
+export type User = {
   id: string
-  amount: number
-  status: "pending" | "processing" | "success" | "failed"
-  email: string
+  username: string
+  email: string,
+  name: {
+    firstname: string,
+    lastname: string
+  }
 }
 
-export const columns: ColumnDef<Payment>[] = [
+export const columns: ColumnDef<User>[] = [
   {
     id: "select",
     header: ({ table }) => (
@@ -99,10 +69,10 @@ export const columns: ColumnDef<Payment>[] = [
     enableHiding: false,
   },
   {
-    accessorKey: "status",
-    header: "Status",
+    accessorKey: "name",
+    header: "Name",
     cell: ({ row }) => (
-      <div className="capitalize">{row.getValue("status")}</div>
+      <div className="capitalize">{row.getValue("username")}</div>
     ),
   },
   {
@@ -121,18 +91,10 @@ export const columns: ColumnDef<Payment>[] = [
     cell: ({ row }) => <div className="lowercase">{row.getValue("email")}</div>,
   },
   {
-    accessorKey: "amount",
-    header: () => <div className="text-right">Amount</div>,
+    accessorKey: "username",
+    header: () => <div className="text-right">Username</div>,
     cell: ({ row }) => {
-      const amount = parseFloat(row.getValue("amount"))
-
-      // Format the amount as a dollar amount
-      const formatted = new Intl.NumberFormat("en-US", {
-        style: "currency",
-        currency: "USD",
-      }).format(amount)
-
-      return <div className="text-right font-medium">{formatted}</div>
+      return <div className="text-right font-medium">{row.getValue("username")}</div>
     },
   },
   {
@@ -166,7 +128,7 @@ export const columns: ColumnDef<Payment>[] = [
   },
 ]
 
-export function DataTable() {
+export function DataTable({ data }: { data: User[] }) {
   const [sorting, setSorting] = React.useState<SortingState>([])
   const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>(
     []
