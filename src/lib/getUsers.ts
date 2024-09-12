b2864fe15b153570d9a6f7ac92feca5d5ff40f2b
Kirill Dubov <31469909+KirillUnited@users.noreply.github.com>
2024-09-12 18:54:59 +0300

    refactor: :zap: Improve users data fetch

--- src/lib/getUsers.ts
+++ src/lib/getUsers.ts
@@ -1,10 +1,15 @@
 export default async function getUsers() {
+    const url = `${process.env.NEXT_PUBLIC_DOMAIN}/api/users`;
     const result: Array<any> = [];
 
     try {
-        const data = await fetch(`${process.env.NEXT_PUBLIC_DOMAIN}/api/users`, {
+        const res = await fetch(url, {
             method: "GET",
-        }).then((res) => res.json());
+        });
+
+        if (!res.ok) throw new Error("Failed to fetch data: " + res.statusText);
+
+        const data = await res.json();
 
         result.push(...data);
     } catch (error) {
