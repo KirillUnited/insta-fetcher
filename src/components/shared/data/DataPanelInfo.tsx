28c75bcf32605a4687d43ad432e9f5802e506b85
Kirill Dubov <31469909+KirillUnited@users.noreply.github.com>
2024-09-24 19:02:48 +0300

    style: :lipstick: Update 'Posts' icon

--- src/components/shared/data/DataPanelInfo.tsx
+++ src/components/shared/data/DataPanelInfo.tsx
@@ -1,5 +1,5 @@
 import { cn } from '@/lib/utils';
-import { PodcastIcon, User2Icon } from 'lucide-react';
+import { MessageSquareTextIcon, User2Icon } from 'lucide-react';
 import React from 'react'
 
 interface Props extends React.HtmlHTMLAttributes<HTMLElement> {
@@ -15,7 +15,7 @@ const Item = ({ title, count, className }: { title: string, count: number } & Re
         className
     )}>
         <div className='flex items-center justify-center flex-shrink-0 w-10 h-10 md:w-16 md:h-16 rounded-full bg-primary/30'>
-            {title === 'Followers' ? <User2Icon className='text-primary w-5 h-5 md:w-10 md:h-10' /> : <PodcastIcon className='text-primary w-5 h-5 md:w-10 md:h-10' />}
+            {title === 'Followers' ? <User2Icon className='text-primary w-5 h-5 md:w-10 md:h-10' /> : <MessageSquareTextIcon className='text-primary w-5 h-5 md:w-10 md:h-10' />}
         </div>
         <div className='flex flex-col gap-1 overflow-hidden'>
             <p className='text-primary text-sm'>{title}</p>
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,8 +1,8 @@
 export const INFO = {
-    url: 'https://www.instagram.com/san_vino/',
-    avatar: '/images/account-avatar.jpg',
-    name: '@san_vino',
-    title: 'San Vino',
-    followers: 1234,
-    posts: 2345,
-  }
+  url: 'https://www.instagram.com/san_vino/',
+  avatar: '/images/account-avatar.jpg',
+  name: '@san_vino',
+  title: 'San Vino',
+  followers: 1234,
+  posts: 2345,
+}
